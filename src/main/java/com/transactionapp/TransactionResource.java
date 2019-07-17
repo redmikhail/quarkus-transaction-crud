975e326076014b110444b249d410a938685d3e07
Sadhana Nandakumar <snandaku@redhat.com>
2019-07-17 15:21:48 -0400

    dependency fixes

--- src/main/java/com/transactionapp/TransactionResource.java
+++ src/main/java/com/transactionapp/TransactionResource.java
@@ -1,20 +1,18 @@
 package com.transactionapp;
 
+import java.util.List;
+
+import javax.transaction.Transactional;
+import javax.validation.Valid;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
 import io.quarkus.panache.common.Sort;
-
-import java.util.List;
-
-import javax.transaction.Transactional;
-import javax.validation.Valid;
 
 @Path("/transaction")
 @Produces("application/json")
@@ -22,11 +20,7 @@ import javax.validation.Valid;
 
 public class TransactionResource {
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "hello";
-    }
+
 
     @POST
     @Transactional
