afaadec03cb8f169d4abf7ef8530c7ae44ef297b
Sadhana Nandakumar <39506194+snandakumar87@users.noreply.github.com>
2019-07-18 11:02:21 -0400

    Update TxnObj.java

--- src/main/java/com/transactionapp/TxnObj.java
+++ src/main/java/com/transactionapp/TxnObj.java
@@ -7,10 +7,35 @@ import javax.persistence.Column;
 
 @Entity
 public class TxnObj extends PanacheEntity {
-    @Column
+    @Column(name = "transaction_id")
     private String transactionNumber;
-    @Column
+    @Column(name = "amount")
     private Integer transactionAmount;
+    @Column(name = "userId")
+    private Integer userId;
+    @Column(name = "last_login")
+    private Date login;
+    
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+    
+   public Date getLogin() {
+        return login;
+    }
+
+    public void setLogin(String userId) {
+        this.login = login;
+    } 
+    
+    
+    
+    
+    
 
     public String getTransactionNumber() {
         return transactionNumber;
